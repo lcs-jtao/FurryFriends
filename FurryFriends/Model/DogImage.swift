4d14c10d2b2584abdec2817a8bf39c7d1c3bd0fd
Joyce Tao <jtao@lcs.on.ca>
2022-03-01 12:39:42 -0500

    Changed CatImage to DogImage and try to load an image from the remote endpoint when the app opens

---
+++ FurryFriends/Model/DogImage.swift
@@ -0,0 +1,13 @@
+//
+//  DogImage.swift
+//  FurryFriends
+//
+//  Created by Joyce Tao on 2022-03-01.
+//
+
+import Foundation
+
+struct DogImage: Decodable {
+    let message: String
+    let status: String
+}
--- FurryFriends/Views/ContentView.swift
+++ FurryFriends/Views/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
     
     // Address for main image
     // Starts as a transparent pixel – until an address for an animal's image is set
-    @State var currentImage = URL(string: "https://www.russellgordon.ca/lcs/miscellaneous/transparent-pixel.png")!
+    
+    @State var currentImage: DogImage = DogImage(message: "https://www.russellgordon.ca/lcs/miscellaneous/transparent-pixel.png", status: "")
     
     @State var inputNotes: String = ""
     
@@ -23,7 +24,7 @@ struct ContentView: View {
         VStack {
             
             // Shows the main image
-            RemoteImageView(fromURL: currentImage)
+            RemoteImageView(fromURL: URL(string: currentImage.message)!)
             
             TextField("Notes", text: $inputNotes, prompt: Text("Enter your notes here"))
             
@@ -57,12 +58,26 @@ struct ContentView: View {
         // Runs once when the app is opened
         .task {
             
-            // Example images for each type of pet
-            let remoteCatImage = "https://purr.objects-us-east-1.dream.io/i/JJiYI.jpg"
+            let remoteDogImage = URL(string: "https://dog.ceo/api/breeds/image/random")!
             
-            // Replaces the transparent pixel image with an actual image of an animal
-            // Adjust according to your preference ☺️
-            currentImage = URL(string: remoteCatImage)!
+            var request = URLRequest(url: remoteDogImage)
+            
+            request.setValue("application/json", forHTTPHeaderField: "Accept")
+            
+            let urlSession = URLSession.shared
+            
+            do {
+                
+                let (data, _) = try await urlSession.data(for: request)
+                
+                currentImage = try JSONDecoder().decode(DogImage.self, from: data)
+                
+            } catch {
+                
+                print("Could not retrieve or decode the JSON from endpoint.")
+                
+                print(error)
+            }
                         
         }
         .navigationTitle("Furry Friends")
