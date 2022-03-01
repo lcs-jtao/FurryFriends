8c7741f08b398c37caa45bc898fb167b875dcb68
Joyce Tao <jtao@lcs.on.ca>
2022-03-01 09:30:02 -0500

    Static interface completed

--- FurryFriends/Views/ContentView.swift
+++ FurryFriends/Views/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
     // Starts as a transparent pixel – until an address for an animal's image is set
     @State var currentImage = URL(string: "https://www.russellgordon.ca/lcs/miscellaneous/transparent-pixel.png")!
     
+    @State var inputNotes: String = ""
+    
     // MARK: Computed properties
     var body: some View {
         
@@ -22,6 +24,8 @@ struct ContentView: View {
             
             // Shows the main image
             RemoteImageView(fromURL: currentImage)
+            
+            TextField("Notes", text: $inputNotes, prompt: Text("Enter your notes here"))
             
             Image(systemName: "heart.circle")
                 .resizable()
@@ -62,6 +66,7 @@ struct ContentView: View {
                         
         }
         .navigationTitle("Furry Friends")
+        .padding()
         
     }
     
